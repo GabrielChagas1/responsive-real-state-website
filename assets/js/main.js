0f0e951f04aebe4660371475562d40d7e5b8e827
Gabriel Chagas <gabrielserq@outlook.com>
2022-05-22 21:10:59 -0300

    chore: creating function toggleItem.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
 var swiperPopular = new Swiper(".popular__container", {
-    spaceBetween: 32,
-    grabCursor: true,
-    centeredSlides: true,
-    slidePerView: 'auto',
-    loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidePerView: 'auto',
+  loop: true,
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.value__accordion-item');
@@ -38,13 +39,25 @@ accordionItems.forEach((item) => {
   })
 });
 
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordion-content');
+
+  if (item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style');
+    item.classList.remove('accordion-open')
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion-open');
+  }
+}
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
