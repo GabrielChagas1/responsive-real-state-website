ce2ed227449173a09de6fd2cb55628ebfebd7fe7
Gabriel Chagas <gabrielserq@outlook.com>
2022-05-25 09:42:55 -0300

    chore: creating js dark light theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,21 @@ const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "bx-sun";
 
-/*=============== DARK LIGHT THEME ===============*/
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
+const getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+    themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](iconTheme);
+}
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
