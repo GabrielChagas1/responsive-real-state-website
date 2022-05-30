a6eafde9da97d34f22e3fb8d6b8debec7662cd17
Gabriel Chagas <gabrielserq@outlook.com>
2022-05-30 09:05:00 -0300

    chore: creating swiper popular js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,14 @@ var swiperPopular = new Swiper(".popular__container", {
   spaceBetween: 32,
   grabCursor: true,
   centeredSlides: true,
-  slidePerView: 'auto',
+  slidesPerView: 'auto',
   loop: true,
 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
 });
 
 /*=============== VALUE ACCORDION ===============*/
